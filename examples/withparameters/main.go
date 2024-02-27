f83fa208e6dc3a88ea0f8ea26c4f8e066a0709d4
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2024-02-27 20:35:29 +0100

    Add example how to schedule function with parameters

---
+++ examples/withparameters/main.go
@@ -0,0 +1,33 @@
+// Example that shows how to schedule task and provide parameters to the
+// scheduled function.
+package main
+
+import (
+	"fmt"
+	"github.com/dl1998/go-scheduler/pkg/scheduler"
+	"os"
+	"time"
+)
+
+func DemoFunction(task *scheduler.Task, name string) {
+	fmt.Printf("Hello, %s!\n", name)
+}
+
+func ScheduleDemoTask(scheduler scheduler.Scheduler) *scheduler.Task {
+	name := "Demo Task"
+	duration := 10 * time.Second
+	interval := time.Second
+
+	return scheduler.ScheduleTask(name, nil, &duration, interval, DemoFunction, os.Getenv("USER"))
+}
+
+func main() {
+	newScheduler := scheduler.New()
+
+	task := ScheduleDemoTask(*newScheduler)
+
+	task.Wait()
+
+	fmt.Println()
+	fmt.Println(task)
+}
