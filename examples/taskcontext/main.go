c223eda31fce0d69ab46c28503127b8e1e1dbe2a
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2024-02-27 20:31:57 +0100

    Add example how to use task context

---
+++ examples/taskcontext/main.go
@@ -0,0 +1,37 @@
+// Example that shows how to use Task context.
+package main
+
+import (
+	"fmt"
+	"github.com/dl1998/go-scheduler/pkg/scheduler"
+	"time"
+)
+
+func DemoFunction(task *scheduler.Task) {
+	contextCounterName := "counter"
+	if task.GetFromContext(contextCounterName) == nil {
+		task.SetToContext(contextCounterName, 0)
+	}
+	counter := task.GetFromContext(contextCounterName).(int)
+	fmt.Printf("[%d] Hello, World!\n", counter)
+	task.SetToContext(contextCounterName, counter+1)
+}
+
+func ScheduleDemoTask(scheduler scheduler.Scheduler) *scheduler.Task {
+	name := "Demo Task"
+	duration := 10 * time.Second
+	interval := time.Second
+
+	return scheduler.ScheduleTask(name, nil, &duration, interval, DemoFunction)
+}
+
+func main() {
+	newScheduler := scheduler.New()
+
+	task := ScheduleDemoTask(*newScheduler)
+
+	task.Wait()
+
+	fmt.Println()
+	fmt.Println(task)
+}
